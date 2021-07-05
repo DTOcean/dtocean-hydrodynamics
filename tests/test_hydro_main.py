0948ee388fcd8d1dcf85f70448e87f878e714cc7
Mathew Topper <damm_horse@yahoo.co.uk>
2021-07-05 17:21:34 +0100

    Minor changes

--- tests/test_hydro_main.py
+++ tests/test_hydro_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#    Copyright (C) 2017-2018 Mathew Topper
+#    Copyright (C) 2017-2021 Mathew Topper
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
